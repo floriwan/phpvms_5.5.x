46fba2110e054d3327f2c58725b75d3fa3168df7
floriwan <nospam@goeldenitz.org>
2016-07-05 22:42:50 +0200

    donation button

--- lib/skins/alpha/layout.php
+++ lib/skins/alpha/layout.php
@@ -185,6 +185,14 @@
         <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
         <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>-->
       </ul>
+      
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="E2AKFB6DUCU8A">
+        <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
+      </form>
+
       <ul class="copyright">
         <li>copyright &copy; 2007 - <?php echo date('Y') ?> - <?php echo SITE_NAME; ?></li>
         <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
